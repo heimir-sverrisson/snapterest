1529ff0e5db9a899a933193ab12689b5b7de1971
Heimir Sverrisson <heimir.sverrisson@gmail.com>
2016-01-16 19:50:40 -0700

    Minimal structure in place

---
+++ app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var h1 = <h1 className="header">This is React</h1>;
+var p = <p className="content">And that's how it works.</p>;
+
+var listOfItems = <ul className="list-of-items">
+                    <li className="item-1">Item 1</li>
+                    <li className="item-2">Item 2</li>
+                    <li className="item-3">Item 3</li>
+		  </ul>;
+
+var reactFragment = [h1, p, listOfItems];
+var section = React.createElement('section', { className: 'container',
+    key: 'container' }, reactFragment);
+
+ReactDOM.render(section, document.getElementById('react-application'));
---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var browserify = require('browserify');
+var babelify = require('babelify');
+var source = require('vinyl-source-stream');
+
+gulp.task('default', function() {
+    return browserify('./source/app.jsx')
+	.transform("babelify", {"presets": ["es2015","react"]})
+	.bundle()
+	.pipe(source('snapterest.js'))
+	.pipe(gulp.dest('./build/'));
+});
+
+// Install with: npm install --save-dev babel-preset-es2015 babel-preset-react
